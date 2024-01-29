136667454501e41ef92c137d9668a3db343c396c
Achcha2001 <113570864+Achcha2001@users.noreply.github.com>
2024-01-29 11:41:03 +0530

    status

--- src/components/interns.js
+++ src/components/interns.js
@@ -410,7 +410,7 @@ const [jobStatus, setJobStatus] = useState([]);
                 <br />
                 <span className="job-property3">Qualifications:</span> {job.qualifications}
                 <br />
-                <span className="job-property3">status:</span> {jobStatus.status}
+                <span className="job-property3">status:pass</span> {jobStatus.status}
                 {/* Display Accepted or Declined message based on job status */}
                 {jobStatus.status === 'Accepted' && <p className="accepted-message">Accepted</p>}
                 {jobStatus.status === 'Declined' && <p className="declined-message">Declined</p>}
